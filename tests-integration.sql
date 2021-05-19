4b402598d0e1f8530ac26c0cbeb98308b866d23b
Motiejus Jakštys <motiejus@jakstys.lt>
2021-05-19 22:57:46 +0300

    demo with island

--- tests-integration.sql
+++ tests-integration.sql
@@ -2,4 +2,4 @@
 
 drop table if exists agg_rivers_wm;
 create table agg_rivers_wm (name text, way geometry);
-insert into agg_rivers_wm (name, way) select name, ST_SimplifyWM_DEBUG(way) from agg_rivers;
+insert into agg_rivers_wm (name, way) select name, ST_SimplifyWM_DEBUG(way) from agg_rivers where name='Nemunas';
